f95e6f34b7f491d8a5d4247211d814362ddd5ea7
Vasiliev Sergei <sergei.binp@gmail.com>
2023-06-19 21:52:44 +0500

    1st commit: first method

--- finish_test_main_block/Program.cs
+++ finish_test_main_block/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arrayGenerate()
+{
+    // Запрос у пользователя размера создаваемого массива
+    Console.WriteLine("Уважаемый пользователь! Введите размер создаваемого массива (кол-во строк): ");
+    int array_size = Convert.ToInt32(Console.ReadLine());
+    // Создание одномерного массива строк нужного размера
+    string[] array = new string[array_size];
+    // Запрос у пользователя ввода элементов массива (построчно) с помощью цикла
+    for(int i = 0; i < array_size; i++) 
+    {
+        Console.WriteLine($"Введите строку номер {i+1}: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+} // Конец метода
+
+string[] myArray = arrayGenerate();
+Console.WriteLine($"{myArray[2]}");
